9ab18532e080b14e4c160568be9b2f6bcec178c4
Osgiliath <mgonzalez@prokarma.com>
2020-02-13 08:10:31 -0700

    font awesome and consumer module

---
+++ src/app/modules/consumer/_components/top-bar/consumer-top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consumer-top-bar',
+  templateUrl: './consumer-top-bar.component.html',
+  styleUrls: ['./consumer-top-bar.component.scss']
+})
+export class ConsumerTopBar implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
