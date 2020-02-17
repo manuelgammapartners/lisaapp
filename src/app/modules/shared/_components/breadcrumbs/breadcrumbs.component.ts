adf1dbcd9f06703a71b1ec260dfa31c15abe4f3a
Cristian Franco <cfranco@prokarma.com>
2020-02-17 14:58:50 -0700

    breadcrum component added

---
+++ src/app/modules/shared/_components/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss']
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+  }
+
+}
