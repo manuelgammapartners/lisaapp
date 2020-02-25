eaf66f89f023b1d18c257f390345c33d9d75a2c5
Manuel Gonzalez <mgonzalez@prokarma.com>
2020-02-25 10:39:23 -0700

    compiled

--- src/app/modules/consumer/_services/consumer.service.ts
+++ src/app/modules/consumer/_services/consumer.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+import { RequestService } from 'src/app/request.service';
 
 
-import { RequestService } from '../../../services/request.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsumerService {
 
-  constructor(private http: RequestService) { }
+  constructor(private _requestService: RequestService) { } 
 
 
   getUpcomingAppts(){
     let url = 'appointments/mine?access_token=s0sPMpmfbFIMGziuYudxpj4OtqD9QWWcS5o3X3LoMQwOaIStjzHcf1C24wo7trip3BJwHeMy0X1dO52ls1R6eUlFiD88h2zj3JtL';
-    return this.http.get(url);
+    return this._requestService.get(url);
   }
 
 
