12db09a0d21b4a7e0a9db925d2a5d4633dda6461
Cristian Franco <cfranco@prokarma.com>
2020-02-26 14:29:26 -0700

    fix conflicts

--- src/app/modules/authentication/_services/auth.service.ts
+++ src/app/modules/authentication/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { RequestService } from 'src/app/services/request.service';
 import { User } from 'src/app/models/user';
 import { Observable } from 'rxjs';
-
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 
 @Injectable({
@@ -10,11 +10,12 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  constructor(private _requestService: RequestService, ) { }
+  constructor(private _requestService: RequestService, private _localStorageService: LocalStorageService) { }
 
 
   postLogin(credentials: any): Observable<User> {
     //return this._requestService.post('auth/web-app/login', credentials)
+    this._localStorageService.storeSession(credentials);
     return this._requestService.postT<User>('auth/web-app/login', credentials);
   }
 
