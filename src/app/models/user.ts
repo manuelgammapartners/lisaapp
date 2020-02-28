53e148c43efc2aee208fafbdcd0b4c11d6f3cb02
Cristian Franco <cfranco@prokarma.com>
2020-02-28 08:05:57 -0700

    models created

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,9 @@
+import Avatar from './avatar';
 export default class User {
     id: number;
     email: string;
     first_name: string;
     last_name: string;
     phone: string;
-    avatar: string;
+    avatar: Avatar;
 }
--- src/app/modules/authentication/_components/login/login.component.ts
+++ src/app/modules/authentication/_components/login/login.component.ts
@@ -5,6 +5,9 @@ import { AuthService } from '../../_services/auth.service';
 import { logging } from 'protractor';
 // import User from 'src/app/models/';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import User from 'src/app/models/user';
+import Artist from 'src/app/models/artist';
+import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
 
 
 
@@ -25,6 +28,7 @@ export class LoginComponent implements OnInit {
   message: string
   type: string
   isLoading: boolean
+
   constructor(private _router: Router, private _authService: AuthService, private _localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
@@ -38,9 +42,10 @@ export class LoginComponent implements OnInit {
     this._localStorageService.cleanLocalStorage();
     this._authService.postLogin(this.loginModel).subscribe(
       data => {
+        this.createSessionUser(data)
         this._localStorageService.storeSession(data);
 
-        this.handleNavigationForUser()
+        //this.handleNavigationForUser()
       },
       error => {
         this.message = error.error.message;
@@ -58,6 +63,48 @@ export class LoginComponent implements OnInit {
     this.login();
   }
 
+  createSessionUser(obj){
+    /*var user = new User();
+    console.log("que trae"+JSON.stringify(obj));
+    switch(obj.type){
+      case 'admin':
+        console.log("es admin"+obj.type);
+        break;
+
+      case 'service manager':
+        console.log("es managaer"+obj.type);
+        break;
+
+      case 'consumer':
+        console.log("es admin"+obj.type);
+        break;
+      case 'artist'
+        console.log();
+        break;
+    }*/
+
+    /*
+
+    if (obj.type.some(e => e.name === 'admin')) {
+      //Todo add this route and uncomment this._router.navigate(['/admin']);
+      //user = new Admin();
+      this.message = "There is not admin module yet, please login as consumer";
+    }
+    else if (obj.type.some(e => e.name === 'service manager')) {
+      //user = new Manager();
+      //Todo add this route and uncomment this._router.navigate(['/manager']);
+      this.message = "There is not manager module yet, please login as consumer";
+    }
+    else if (obj.type.some(e => e.name === 'consumer')) {
+      //user = new Consumer();
+      console.log("entro aca y es un user");
+      this._router.navigate(['/consumer']);
+    } else {
+      console.log("Web app is not for artist");
+    }
+
+    return user;*/
+  }
 
   handleNavigationForUser() {
 
